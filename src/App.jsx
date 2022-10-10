7806dcb42cfc1b7cded3b184b859d4f57944882b
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-10-10 15:31:58 +0700

    add header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div class="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
